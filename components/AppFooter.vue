e343be65feb7c54b7dd34e827661e63226e7eb34
Kieran <kiermccarth+github@gmail.com>
2023-12-04 13:15:47 +0000

    ADD: Blank header/footer components

---
+++ components/AppFooter.vue
@@ -0,0 +1,7 @@
+<template>
+  <footer class="footer">Footer</footer>
+</template>
+
+<style scoped lang="scss"></style>
+
+<script setup lang="ts"></script>
---
+++ components/AppHeader.vue
@@ -0,0 +1,7 @@
+<template>
+  <header class="header">Header</header>
+</template>
+
+<style scoped lang="scss"></style>
+
+<script setup lang="ts"></script>
