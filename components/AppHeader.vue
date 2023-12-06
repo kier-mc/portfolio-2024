5f569bb03c9e30b40fc4f9c1e5c0fe8b64cce119
Kieran <kiermccarth+github@gmail.com>
2023-12-06 16:52:25 +0000

    MODIFY: First pass for content/style

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,7 +1,47 @@
 <template>
-  <header class="header">Header</header>
+  <header class="header">
+    <AppLogo class="logo" background-opacity="0.85" />
+    <nav class="nav">
+      <ul class="links">
+        <li v-for="(link, key) in data.links" :key="key" class="link">
+          <a :href="`#${link.to}`">{{ link.label }}</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+@use "../assets/styles/var/size";
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  backdrop-filter: blur(size.$xs);
+}
+.logo {
+  max-width: size.$header-logo;
+}
+.links {
+  all: unset;
+  display: inline-flex;
+  gap: size.$md;
+}
+.link {
+  all: unset;
+}
+.link > a {
+  all: unset;
+  cursor: pointer;
+}
+</style>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const data = {
+  links: [
+    { label: "Home", to: "anchor-home" },
+    { label: "Projects", to: "anchor-skills" },
+    { label: "Contact", to: "anchor-contact" },
+  ],
+};
+</script>
