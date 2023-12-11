7a35b32dc7908e1e954f2dc7fffa41f829aa1451
Kieran <kiermccarth+github@gmail.com>
2023-12-11 19:52:46 +0000

    ADD: Create AppIcon component to manage SVGs across the app

---
+++ components/AppIcon.vue
@@ -0,0 +1,24 @@
+<template>
+  <component :is="setIcon" />
+</template>
+
+<style scoped lang="scss"></style>
+
+<script setup lang="ts">
+enum Icons {
+  html = "HTML",
+  css = "CSS",
+  ts = "TS",
+}
+
+type ValidIcon = keyof typeof Icons;
+
+const props = defineProps({
+  icon: { type: String as PropType<ValidIcon>, required: true },
+});
+
+const setIcon = computed(() => {
+  const icon = Icons[props.icon];
+  return defineAsyncComponent(() => import(`./icons/Icon${icon}.vue`));
+});
+</script>
