070da884aea004ed878923ad6f81a624f6ae37d9
Kieran <kiermccarth+github@gmail.com>
2024-04-14 11:23:51 +0100

    ADD: Add accessible description tag

--- components/icons/IconJS.vue
+++ components/icons/IconJS.vue
@@ -1,5 +1,6 @@
 <template>
   <svg viewBox="0 0 128 128">
+    <title>JavaScript logo</title>
     <path fill="#F0DB4F" d="M1.408 1.408h125.184v125.185H1.408z"></path>
     <path
       fill="#323330"
