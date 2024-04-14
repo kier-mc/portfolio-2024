fe1ca1a05ae583b60f2fc878c6fb8fe981889dd3
Kieran <kiermccarth+github@gmail.com>
2024-04-14 14:25:29 +0100

    REFACTOR: Move to dedicated component subdirectory

---
+++ components/content/ContentIntro.vue
@@ -0,0 +1,24 @@
+<template>
+  <article class="introduction">
+    <h2>Introduction</h2>
+    <p>
+      I'm Kieran, a web developer from the UK with strong front end skills but a good knowledge of how things
+      work across the stack, from design to database. I specialise in clean, responsive and maintainable code,
+      where the quality speaks for itself.
+    </p>
+    <p>
+      I love to build and will work on anything, as long as it contributes toward making the world a better
+      place.
+    </p>
+  </article>
+</template>
+
+<style scoped lang="scss">
+.introduction {
+  display: grid;
+  grid-auto-rows: fit-content;
+  row-gap: var(--sz-sm);
+}
+</style>
+
+<script setup lang="ts"></script>
