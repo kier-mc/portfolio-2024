99fcf9b4c8b0004e4ff96d7eb76b7a8654d6a38c
Kieran <kiermccarth+github@gmail.com>
2024-04-14 14:24:56 +0100

    REFACTOR: Move to dedicated component subdirectory

---
+++ components/app/AppIcon.vue
@@ -0,0 +1,18 @@
+<template>
+  <component :is="setIcon" />
+</template>
+
+<style scoped lang="scss"></style>
+
+<script setup lang="ts">
+export type ValidIcon = keyof typeof ValidIcons;
+
+const props = defineProps({
+  icon: { type: String as PropType<ValidIcon>, required: true },
+});
+
+const setIcon = computed(() => {
+  const icon = ValidIcons[props.icon];
+  return defineAsyncComponent(() => import(`~/components/icons/Icon${icon}.vue`));
+});
+</script>
