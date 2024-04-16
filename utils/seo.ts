a38562279b3c56b9871a949ad2eca9ed74f07378
Kieran <kiermccarth+github@gmail.com>
2024-04-16 22:15:54 +0100

    ADD: SEO util

---
+++ utils/seo.ts
@@ -0,0 +1,8 @@
+import type { UseSeoMetaInput } from "@unhead/schema";
+
+export const seoMetadata: UseSeoMetaInput = {
+  title:          "Kieran McCarth - Web Developer Portfolio",
+  ogTitle:        "Kieran McCarth - Web Developer Portfolio",
+  description:    "The digital portfolio of Kieran McCarth, web developer. Includes skills, frameworks and competencies, as well as links and references to recent projects and work.",
+  ogDescription:  "The digital portfolio of Kieran McCarth, web developer. Includes skills, frameworks and competencies, as well as links and references to recent projects and work.",
+};
