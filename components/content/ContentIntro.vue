a1621ce0ff18a53415207407a5bf22ae21a5c3cc
Kieran <kiermccarth+github@gmail.com>
2024-04-16 20:39:02 +0100

    REFACTOR: Wrap paragraphs in <div> for more granular styling

--- components/content/ContentIntro.vue
+++ components/content/ContentIntro.vue
@@ -1,15 +1,17 @@
 <template>
   <article class="introduction">
     <h2>Introduction</h2>
-    <p>
-      I'm Kieran, a web developer from the UK with strong front end skills but a good knowledge of how things
-      work across the stack, from design to database. I specialise in clean, responsive and maintainable code,
-      where the quality speaks for itself.
-    </p>
-    <p>
-      I love to build and will work on anything, as long as it contributes toward making the world a better
-      place.
-    </p>
+    <div class="introduction__content">
+      <p>
+        I'm Kieran, a web developer from the UK with strong front end skills but a good knowledge of how
+        things work across the stack, from design to database. I specialise in clean, responsive and
+        maintainable code, where the quality speaks for itself.
+      </p>
+      <p>
+        I love to build and will work on anything, as long as it contributes toward making the world a better
+        place.
+      </p>
+    </div>
   </article>
 </template>
 
@@ -17,7 +19,11 @@
 .introduction {
   display: grid;
   grid-auto-rows: fit-content;
-  row-gap: var(--sz-sm);
+  row-gap: var(--sz-2xs);
+  &__content {
+    display: grid;
+    row-gap: var(--sz-md);
+  }
 }
 </style>
 
